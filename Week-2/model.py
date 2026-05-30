797af7c10dbdda87c58cd6243f809453f33f3bd8
Arhan <floorgangforever007@gmail.com>
2026-05-30 11:13:52 +0530

    feat: enhance UNet architecture with configurable depth level, skip connections and final activation

--- Week-2/model.py
+++ Week-2/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from blocks import DoubleConv, Down, Up
 
+
 class UNet(nn.Module):
 
     def __init__(
@@ -10,11 +11,15 @@ class UNet(nn.Module):
         in_channels=1,
         out_channels=1,
         base_channels=64,
-        depth=4
+        depth=4,
+        use_skip_connections=True,
+        final_activation=None
     ):
         super().__init__()
 
         self.depth = depth
+        self.use_skip_connections = use_skip_connections
+        self.final_activation = final_activation
 
         channels = [
             base_channels * (2 ** i)
@@ -30,15 +35,26 @@ class UNet(nn.Module):
 
         for i in range(depth):
             self.downs.append(
-                Down(channels[i], channels[i+1])
+                Down(
+                    channels[i],
+                    channels[i + 1]
+                )
             )
 
         self.ups = nn.ModuleList()
 
         for i in reversed(range(depth)):
+
+            if use_skip_connections:
+                up_in_channels = (
+                    channels[i + 1] + channels[i]
+                )
+            else:
+                up_in_channels = channels[i + 1]
+
             self.ups.append(
                 Up(
-                    channels[i+1] + channels[i],
+                    up_in_channels,
                     channels[i]
                 )
             )
@@ -62,7 +78,29 @@ class UNet(nn.Module):
 
         skips = skips[:-1][::-1]
 
-        for up, skip in zip(self.ups, skips):
-            x = up(x, skip)
+        if self.use_skip_connections:
 
-        return self.outc(x)
+            for up, skip in zip(self.ups, skips):
+                x = up(x, skip)
+
+        else:
+
+            for up in self.ups:
+                x = up(x, None)
+
+        x = self.outc(x)
+
+        if self.final_activation == "sigmoid":
+            x = torch.sigmoid(x)
+
+        elif self.final_activation == "tanh":
+            x = torch.tanh(x)
+
+        return x
+
+    def get_config(self):
+        return {
+            "depth": self.depth,
+            "use_skip_connections": self.use_skip_connections,
+            "final_activation": self.final_activation
+        }
