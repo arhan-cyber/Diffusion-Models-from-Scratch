e006348193aa494b49f2f08d41a82652067276e3
Arhan <floorgangforever007@gmail.com>
2026-05-30 11:20:03 +0530

    blocks.py now handles there not being skipping as well

--- Week-2/blocks.py
+++ Week-2/blocks.py
@@ -1,80 +1,153 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        use_batchnorm=True
+    ):
         super().__init__()
 
-        self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels,
-                      kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+        layers = [
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=3,
+                padding=1,
+                bias=not use_batchnorm
+            )
+        ]
 
-            nn.Conv2d(out_channels, out_channels,
-                      kernel_size=3, padding=1),
+        if use_batchnorm:
+            layers.append(
+                nn.BatchNorm2d(out_channels)
+            )
+
+        layers.append(
             nn.ReLU(inplace=True)
         )
+
+        layers.append(
+            nn.Conv2d(
+                out_channels,
+                out_channels,
+                kernel_size=3,
+                padding=1,
+                bias=not use_batchnorm
+            )
+        )
+
+        if use_batchnorm:
+            layers.append(
+                nn.BatchNorm2d(out_channels)
+            )
+
+        layers.append(
+            nn.ReLU(inplace=True)
+        )
+
+        self.block = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.block(x)
 
 
 class Down(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        use_batchnorm=True
+    ):
         super().__init__()
 
         self.block = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                use_batchnorm=use_batchnorm
+            )
         )
 
     def forward(self, x):
         return self.block(x)
-    
-import torch.nn.functional as F
+
 
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels,
-                 bilinear=True):
-
+    def __init__(
+        self,
+        decoder_channels,
+        skip_channels,
+        out_channels,
+        use_skip_connections=True,
+        use_batchnorm=True,
+        bilinear=True
+    ):
         super().__init__()
 
+        self.use_skip_connections = use_skip_connections
+
         if bilinear:
+
             self.up = nn.Upsample(
                 scale_factor=2,
-                mode='bilinear',
+                mode="bilinear",
                 align_corners=True
             )
+
         else:
-            self.up = nn.ConvTranspose2d(
-                in_channels // 2,
-                in_channels // 2,
-                kernel_size=2,
-                stride=2
+            raise NotImplementedError(
+                "ConvTranspose2d mode is not currently "
+                "supported in this implementation."
             )
 
+        conv_in_channels = decoder_channels
+
+        if use_skip_connections:
+            conv_in_channels += skip_channels
+
         self.conv = DoubleConv(
-            in_channels,
-            out_channels
+            in_channels=conv_in_channels,
+            out_channels=out_channels,
+            use_batchnorm=use_batchnorm
         )
 
-    def forward(self, x1, x2):
-
+    def forward(
+        self,
+        x1,
+        x2=None
+    ):
         x1 = self.up(x1)
 
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
+        if self.use_skip_connections:
 
-        x1 = F.pad(
-            x1,
-            [
-                diffX // 2,
-                diffX - diffX // 2,
-                diffY // 2,
-                diffY - diffY // 2
-            ]
-        )
+            if x2 is None:
+                raise ValueError(
+                    "Skip connections enabled but "
+                    "skip tensor is None."
+                )
 
-        x = torch.cat([x2, x1], dim=1)
+            diffY = x2.size(2) - x1.size(2)
+            diffX = x2.size(3) - x1.size(3)
 
-        return self.conv(x)
+            x1 = F.pad(
+                x1,
+                [
+                    diffX // 2,
+                    diffX - diffX // 2,
+                    diffY // 2,
+                    diffY - diffY // 2
+                ]
+            )
+
+            x1 = torch.cat(
+                [x2, x1],
+                dim=1
+            )
+
+        return self.conv(x1)
